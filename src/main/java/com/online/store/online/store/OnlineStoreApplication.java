12668cd7c4d1b0d7de247587345da839379f0bf9
Raju_Nakkanadoddi <raju.n@techurate.com>
2025-04-29 21:04:34 +0200

    enabling scheduler

--- src/main/java/com/online/store/online/store/OnlineStoreApplication.java
+++ src/main/java/com/online/store/online/store/OnlineStoreApplication.java
@@ -2,8 +2,10 @@ package com.online.store.online.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class OnlineStoreApplication {
 
 	public static void main(String[] args) {
